dcc6617746d55b489be09b05ce665b4139d712fd
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-08-09 23:59:58 +0500

    set up basic nextjs project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <h1 className="text-xl font-extrabold text-center text-slate-800">Hello, I am Abdullah and this is my first Next.js project.</h1>
+  );
+}
